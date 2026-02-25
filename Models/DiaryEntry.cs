f0285bac9861e6af257d7d1270507d3e0e1ad7f6
Itsoya Abebe <itsoyaa@gmail.com>
2026-02-25 12:23:35 -0500

    app set up

---
+++ Models/DiaryEntry.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DairyApp.Models
+{
+    public class DiaryEntry
+    {
+        [Key]
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Content { get; set; }
+        public required DateTime Created { get; set; } = DateTime.Now;
+
+    }
+}
