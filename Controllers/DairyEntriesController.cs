d9b7e3c88acccd433c0296684e20a7e49d92261c
Itsoya Abebe <itsoyaa@gmail.com>
2026-02-26 08:39:41 -0500

    update diary entry controller

--- Controllers/DairyEntriesController.cs
+++ Controllers/DairyEntriesController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DairyApp.Data;
+using DairyApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DairyApp.Controllers
 {
     public class DairyEntriesController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public DairyEntriesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<DiaryEntry> entries = _db.DiaryEntries.ToList();
+            return View(entries);
         }
     }
 }
