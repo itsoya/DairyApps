d65fe8bb27609672bac3cb5d5c411cb3e54d73bf
Itsoya Abebe <itsoyaa@gmail.com>
2026-02-26 13:27:49 -0500

    update dairyentry controller

--- Controllers/DairyEntriesController.cs
+++ Controllers/DairyEntriesController.cs
@@ -21,5 +21,39 @@ namespace DairyApp.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(DiaryEntry entry)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.DiaryEntries.Add(entry);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "DairyEntries");
+            }
+            return View(entry);
+        }
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var entry = _db.DiaryEntries.Find(id);
+            if (entry != null)
+            {
+                _db.DiaryEntries.Remove(entry);
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index", "DairyEntries");
+        }
+        [HttpPut]
+        public IActionResult Update(int id, DiaryEntry updatedEntry)
+        {
+            var entry = _db.DiaryEntries.Find(id);
+            if (entry != null)
+            {
+                entry.Title = updatedEntry.Title;
+                entry.Content = updatedEntry.Content;
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index", "DairyEntries");
+        }
     }
 }
